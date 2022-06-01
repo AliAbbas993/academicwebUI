b58f797a75381f7b67a09f5d8f506aa6d87b312d
aliabbas993 <ali.abbas@digitallandscape.com.pk>
2022-06-02 02:07:04 +0500

    writer listing page view added

---
+++ teams.php
@@ -0,0 +1,127 @@
+<?php include'./src/includes/header.php'?>
+<section class="teams-banner-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 d-flex flex-wrap justify-content-start">
+                <div class="col-md-6">
+                    <h1>Our Team of Experienced Writers</h1>
+                    <p>First we check the web. Secondly, we check our own database ofpleted orders so you know you’ll have iginal work every time. f completed orders so you know you’ll have original work every time.</p>
+                    <div class="btn-block d-flex flex-wrap justify-content-start">
+                        <button class="btn btn-white my-3 mx-2">Hire an Experts</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="team-listing-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 p-0">
+                <div class="listing-filter-block">
+                    <form action="">
+                        <div class="d-flex px-3 justify-content-start align-items-center">
+                            <div class="form-group m-0 pr-3">
+                                <select id="writer" name="writer" class="form-control">
+                                    <option>Select Subject</option>
+                                    <option value="Ali">Ali</option>
+                                    <option value="Asad">Asad</option>
+                                    <option value="Sohail">Sohail</option>
+                                    <option value="Saad">Saad</option>
+                                    <option value="Hassan">Hassan</option>
+                                </select>
+                            </div>
+                            <div class="form-group m-0 btn-block">
+                                <button type="submit" class="btn btn-light-blue">Reset All</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="teams-listing-block">
+                    <ul class="list-unstyled m-0 d-flex flex-wrap justify-content-start">
+                        <li class="my-3 col-lg-3">
+                            <div class="d-inline-flex flex-column justify-content-between align-items-center">
+                                <div class="writer-meta d-inline-flex flex-column justify-content-center align-items-center w-100">
+                                    <span class="d-inline-block writer-img"><img src="./assets/media/writer-male.png" alt="writer"></span>
+                                    <p>
+                                        <span class="rating-start"><img src="./assets/media/rating-star.png" alt=""></span>
+                                        <span class="rating-count">4.8</span>
+                                        <span class="reviews-count">381 Reviews</span>
+                                    </p>
+                                </div>
+                                <div class="order-count w-100 d-inline-flex flex-wrap justify-content-between">
+                                    <span>Total Orders</span><span>11325</span>
+                                </div>
+                                <div class="btn-block d-flex flex-wrap justify-content-between">
+                                    <button class="btn btn-light m-1">About Writer</button>
+                                    <button class="btn btn-blue m-1">Hire Writer</button>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="my-3 col-lg-3">
+                            <div class="d-inline-flex flex-column justify-content-between align-items-center">
+                                <div class="writer-meta d-inline-flex flex-column justify-content-center align-items-center w-100">
+                                    <span class="d-inline-block writer-img"><img src="./assets/media/writer.png" alt="writer"></span>
+                                    <p>
+                                        <span class="rating-start"><img src="./assets/media/rating-star.png" alt=""></span>
+                                        <span class="rating-count">4.8</span>
+                                        <span class="reviews-count">381 Reviews</span>
+                                    </p>
+                                </div>
+                                <div class="order-count w-100 d-inline-flex flex-wrap justify-content-between">
+                                    <span>Total Orders</span><span>11325</span>
+                                </div>
+                                <div class="btn-block d-flex flex-wrap justify-content-between">
+                                    <button class="btn btn-light m-1">About Writer</button>
+                                    <button class="btn btn-blue m-1">Hire Writer</button>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="my-3 col-lg-3">
+                            <div class="d-inline-flex flex-column justify-content-between align-items-center">
+                                <div class="writer-meta d-inline-flex flex-column justify-content-center align-items-center w-100">
+                                    <span class="d-inline-block writer-img"><img src="./assets/media/writer-male.png" alt="writer"></span>
+                                    <p>
+                                        <span class="rating-start"><img src="./assets/media/rating-star.png" alt=""></span>
+                                        <span class="rating-count">4.8</span>
+                                        <span class="reviews-count">381 Reviews</span>
+                                    </p>
+                                </div>
+                                <div class="order-count w-100 d-inline-flex flex-wrap justify-content-between">
+                                    <span>Total Orders</span><span>11325</span>
+                                </div>
+                                <div class="btn-block d-flex flex-wrap justify-content-between">
+                                    <button class="btn btn-light m-1">About Writer</button>
+                                    <button class="btn btn-blue m-1">Hire Writer</button>
+                                </div>
+                            </div>
+                        </li>                        
+                        <li class="my-3 col-lg-3">
+                            <div class="d-inline-flex flex-column justify-content-between align-items-center">
+                                <div class="writer-meta d-inline-flex flex-column justify-content-center align-items-center w-100">
+                                    <span class="d-inline-block writer-img"><img src="./assets/media/writer.png" alt="writer"></span>
+                                    <p>
+                                        <span class="rating-start"><img src="./assets/media/rating-star.png" alt=""></span>
+                                        <span class="rating-count">4.8</span>
+                                        <span class="reviews-count">381 Reviews</span>
+                                    </p>
+                                </div>
+                                <div class="order-count w-100 d-inline-flex flex-wrap justify-content-between">
+                                    <span>Total Orders</span><span>11325</span>
+                                </div>
+                                <div class="btn-block d-flex flex-wrap justify-content-between">
+                                    <button class="btn btn-light m-1">About Writer</button>
+                                    <button class="btn btn-blue m-1">Hire Writer</button>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="btn-block my-5 d-flex flex-wrap justify-content-center">
+                    <button class="btn btn-blue m-1">Load More</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include'./src/includes/footer.php'?>
