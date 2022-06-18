3baefb9eaaac97ed6767e96da0c4f035fb1ba24d
hiba <hibakhan130999@gmail.com>
2022-06-19 02:06:02 +0500

    connected to live db.

--- src/includes/db-connection.php
+++ src/includes/db-connection.php
@@ -1,10 +1,14 @@
 <?php
-    $db_host = "localhost"; 
-    $db_user = "root";
-    $db_password = "";
-    $db_name = "shoreditech";
+   $db_host = "181.214.31.161"; 
+   $db_user = "shore_shore";
+   $db_password = "Karachi@111";
+   $db_name = "shore_shoreditchtech";
 
     $con = mysqli_connect($db_host,$db_user,$db_password,$db_name);
     if(mysqli_connect_error()){
         echo "Failed to connect to MySQL";
     }
+    else {
+        echo "Connected";
+    }
+?>
