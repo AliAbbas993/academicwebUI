c11503ffe04b90eb6c43c57efdfdc5779f5554e5
hiba <hiba@globalresourcesca.com>
2022-09-21 08:26:35 +0500

    order preview page ui done

--- order-preview.php
+++ order-preview.php
@@ -1,9 +1,141 @@
 <?php include'./src/includes/header.php'?>
-<section class="pricing-banner-sec">
+<section class="order-preview-banner-sec">
     <div class="container">
         <div class="row">
             <div class="col-md-12 d-flex flex-wrap justify-content-center">
                 <h1>Order Preview</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="order-preview-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="thank-you p-5 mb-5">
+                    <img src="" alt="">
+                    <div>
+                        <h3>THANK YOU</h3>
+                        <p class="text-center">For your order</p>
+                    </div>
+                </div>
+                <h5 class="yellow-heading mb-5">Invoice#1235667</h5> 
+                <div>
+                    <div class="row m-0">
+                        <div class="col-sm-6">
+                            <span>Website:</span>
+                        </div>
+                        <div class="col-sm-6">
+                            <span>www.xyz.com</span>
+                        </div>
+                    </div><hr>
+                    <div class="row m-0">
+                        <div class="col-sm-6">
+                            <span>Email:</span>
+                        </div>
+                        <div class="col-sm-6">
+                            <span>sp15bs0061@maju.edu.pk</span>
+                        </div>
+                    </div><hr>
+                    <div class="row m-0">
+                        <div class="col-sm-6">
+                            <span>Phone:</span>
+                        </div>
+                        <div class="col-sm-6">
+                            <span>923331231231</span>
+                        </div>
+                    </div>
+                </div>
+                <h5 class="yellow-heading my-5">User Details</h5> 
+                <div>
+                    <div class="row m-0">
+                        <div class="col-sm-6">
+                            <span>Name:</span>
+                        </div>
+                        <div class="col-sm-6">
+                            <span>Test</span>
+                        </div>
+                    </div><hr>
+                    <div class="row m-0">
+                        <div class="col-sm-6">
+                            <span>Email:</span>
+                        </div>
+                        <div class="col-sm-6">
+                            <span>sp15bs0061@maju.edu.pk</span>
+                        </div>
+                    </div><hr>
+                    <div class="row m-0">
+                        <div class="col-sm-6">
+                            <span>Phone:</span>
+                        </div>
+                        <div class="col-sm-6">
+                            <span>923331231231</span>
+                        </div>
+                    </div>
+                </div>
+                <h5 class="yellow-heading my-5">Order Summary Details</h5> 
+                <div>
+                    <div class="row m-0">
+                        <div class="col-sm-6">
+                            <span>Experience Level:</span>
+                        </div>
+                        <div class="col-sm-6">
+                            <span>Entry</span>
+                        </div>
+                    </div><hr>
+                    <div class="row m-0">
+                        <div class="col-sm-6">
+                            <span>Service Type:</span>
+                        </div>
+                        <div class="col-sm-6">
+                            <span>Resume Writting</span>
+                        </div>
+                    </div><hr>
+                    <div class="row m-0">
+                        <div class="col-sm-6">
+                            <span>Deadline:</span>
+                        </div>
+                        <div class="col-sm-6">
+                            <span>5 days</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="blue-heading my-4">
+                    <span>Total Amount</span>
+                    <span>$890</span> 
+                </div>
+                <div class="payment-details my-5 pb-3">
+                    <p class="title">Payment Detail</p>
+                    <p>Click Below On The Following Link To Make Payment</p>
+                    <button class="btn-blue">Pay Now</button>
+                    <p class="note my-4">Note: product will be deliver through email.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="side-card">
+                    <div class="rating py-3">
+                        <p>Our clients</p>
+                        <h4>Rating</h4>
+                        <img src="./assets/media/rating-star.png" alt="">
+                    </div>
+                    <p class="rating-value my-2">Rating Value 4.8 Out Of 5</p>
+                    <div class="payment-methods">
+                        <p class="blue-para p-2 m-0">Last Updated On 24-sep-21</p>
+                        <p class="yellow-para p-2 m-0">We accept</p>
+                        <div class="d-flex justify-content-center">
+                            <span>
+                                <img src="./assets/media/visa.png" alt="">
+                            </span>
+                            <span>
+                                <img src="./assets/media/master-card.png" alt="">
+                            </span>
+                        </div>
+                        <p class="yellow-para p-2 m-0">Our guarantee</p>
+                        <span>
+                            <img src="./assets/media/batch.png" alt="" class="p-4">
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
